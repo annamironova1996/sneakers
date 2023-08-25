7fecfa73110918e8e8b3f6872bf08f222e3982d8
annamironova1996 <annamironova19.96@mail.ru>
2023-08-24 22:26:04 -0700

    create new react-project - sneakers-shop

--- src/App.js
+++ src/App.js
@@ -1,23 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="header-wrapper">
+      <header className="header">
+        <div className="header-left">
+          <img
+            src="/img/logo.png"
+            className="mr-15"
+            width={40}
+            height={40}
+          />
+          <div>
+            <h3 className="text-uppercase">sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="header-right">
+          <li>
+            <img
+              width={18}
+              height={18}
+              alt=""
+              src="/img/cart.svg"
+            />
+            <span className="ml-10">1205 руб.</span>
+          </li>
+          <li>
+            <img
+              src="/img/favorites.svg"
+              alt=""
+              width={18}
+              height={18}
+            />
+          </li>
+          <li>
+            <img
+              alt=""
+              src="/img/user.svg"
+              width={18}
+              height={18}
+            />
+          </li>
+        </ul>
       </header>
+      <div className="contant">
+        <h1>Все кроссовки</h1>
+
+        <div className="d-flex">
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/1.jpg"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="card-description">
+              <div className="card-price">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={32}
+                  height={32}
+                  src="/img/plus.svg"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/2.jpg"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="card-description">
+              <div className="card-price">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={32}
+                  height={32}
+                  src="/img/plus.svg"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/3.jpg"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="card-description">
+              <div className="card-price">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={32}
+                  height={32}
+                  src="/img/plus.svg"
+                />
+              </button>
+            </div>
+          </div>
+          <div className="card">
+            <img
+              width={133}
+              height={112}
+              src="/img/sneakers/4.jpg"
+            />
+            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
+            <div className="card-description">
+              <div className="card-price">
+                <span>Цена:</span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img
+                  width={32}
+                  height={32}
+                  src="/img/plus.svg"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
